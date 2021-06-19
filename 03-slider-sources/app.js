373bff2433467ff00a3399d91f0fa3e393e215b8
nobiart <dmitri.besleaga@gmail.com>
2021-06-19 13:27:51 +0300

    #Day 3. Slider. Bonus

--- 03-slider-sources/app.js
+++ 03-slider-sources/app.js
@@ -10,10 +10,18 @@ sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
 
 upBtn.addEventListener('click', () => {
     changeSlide('up');
-})
+});
 
 downBtn.addEventListener('click', () => {
     changeSlide('down');
+});
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up');
+    } else if (event.key === 'ArrowDown') {
+        changeSlide('down');
+    }
 })
 
 function changeSlide(direction) {
